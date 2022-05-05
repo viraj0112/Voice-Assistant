897bf86d0f31992d4e42db72813726f1740319ca
viraj0112 <83603367+viraj0112@users.noreply.github.com>
2022-05-05 16:07:44 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,7 +5,7 @@ from winsound import PlaySound
 
 from setuptools import Command
 import speech_recognition as sr
-import wikipedia, os, datetime, pyttsx3, webbrowser, keyboard, pyjokes
+import wikipedia, os, datetime, pyttsx3, webbrowser, keyboard, pyjokes,sys
 from tkinter import *
 from PIL import Image,ImageTk
 from playsound import playsound 
@@ -14,6 +14,7 @@ import keyboard
 myName = "nisarga's assistant"
 # sapi5-microsoft assistant
 engine = pyttsx3.init('sapi5')
+engine.setProperty("rate",300)
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
 
@@ -57,11 +58,36 @@ def hearme():
     try:
         speak('recognizing your command')  # speak
         query = r.recognize_google(audio,language='en-in')
+        statement=r.recognize_google(audio,language='en-in')
+        print(statement)
         print(query)
         # r.pause_threshold=1 
-           
+
+        if ('google' or 'chrome') in statement.lower():
+            os.system("start chrome")
+            speak("Chrome is now open")
+
         
-        if query.lower().startswith("open") and "." in query.lower():
+        if 'joke' in statement.lower():
+            speak(pyjokes.get_joke())
+        if 'youtube' in statement.lower():
+                webbrowser.open("https://www.youtube.com")
+                speak("youtube is open now")
+        if 'gmail' in statement.lower():
+                webbrowser.open("https://www.gmail.com")
+                speak("gmail is open now")
+        if 'Facebook' in statement.lower():
+                webbrowser.open("https://www.Facebook.com")
+                speak("Facebook is open now")
+        if 'gaana' in statement.lower():
+                webbrowser.open("https://www.gaana.com")
+                speak("gaana is open now")
+        if 'whatsapp' in statement.lower():
+                # os.system("start whatsapp")
+                os.system("start chrome web.whatsapp.com ")
+                speak("whatsapp is open now")
+        
+        elif query.lower().startswith("open") and "." in query.lower():
             res = query.split()
             print(res[1:])
             res = res[1:]
@@ -106,27 +132,28 @@ def main_screen():
     global scr
     scr= Tk()
     scr.title("Thunder")
-    scr.geometry("100x250")
-    # scr.iconphoto("mic.png")
+    scr.geometry("250x150")
+    scr.config(background = "black")
+    # scr.iconphoto("mic.gif")
 
-    name_label=Label(text="Thunder",width=300,bg="black",fg="white",font=("Calibri",13))
+    name_label=Label(text="Thunder",width=300,bg="white",fg="blue",font=("Calibri",13))
     name_label.pack()
 
-    microphone_photo = PhotoImage(file = "mic.png")
+    microphone_photo = PhotoImage(file = "mic.gif")
     # microphone_button = Button(image=microphone_photo,command=wishme)
-    microphone_button = Button(image=microphone_photo,command=hearme)
+    microphone_button = Button(image=microphone_photo,command=hearme, borderwidth = 0)
     microphone_button.pack(pady=10)
 
-    image=Image.open("mic.png")
-    resize_image=image.resize((20,30))
-    img=ImageTk.PhotoImage(resize_image)
-    label1=Label(image=img)
-    label1.image=img
-    label1.pack()
+    # image=Image.open("mic.png")
+    # resize_image=image.resize((20,30))
+    # img=ImageTk.PhotoImage(resize_image)
+    # label1=Label(image=img)
+    # label1.image=img
+    # label1.pack()
 
 
-    settings_photo = PhotoImage(file = "setting.png")
-    settings_button = Button(image=settings_photo)
+    settings_photo = PhotoImage(file = "setting.gif")
+    settings_button = Button(image = settings_photo, borderwidth = 0)
     settings_button.pack(pady=50)
 
     info_button = Button(text ="Info")
@@ -137,14 +164,11 @@ def main_screen():
     
 
 keyboard.add_hotkey("F4",hearme)
-main_screen()
+
 
 
 if __name__ == "__main__":
-    
-    while True:
-        # wishme()
-        hearme()
+    main_screen()
     
    
    
